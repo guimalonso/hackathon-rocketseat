0b2192d08df7e749a350d15f7735ead55890d2f4
Fabio Luis Pereira <zeta@MacBook-Pro-de-Fabio.local>
2019-11-26 23:09:11 -0300

    questions

--- src/pages/Quiz/index.js
+++ src/pages/Quiz/index.js
@@ -1,33 +1,53 @@
-import React from 'react';
+import React, { Component } from "react";
 
-// import { Container } from './styles';
+import { ImageFrame, QuizFrame, Question, Answers, Code } from "./styles";
 
-export default function Quiz() {
-  return (
-    <>
-      <ImageFrame>
-        <img src="" alt="" />
-        <img src="" alt="" />
-      </ImageFrame>
-      <QuizFrame>
-        <Question>
-          <p>1) Complete a função para somar dois números:</p>
-        </Question>
-        <Answers>
-          <li>a) 2 + 2</li>
-          <li>b) retorne 2 + 2</li>
-          <li>c) a = 2 + 2</li>
-          <li>d) [2 + 2]</li>
-        </Answers>
-        <Code>
-          soma(a,b)
-          início
-          <input type="text" />
-          fim
-        </Code>
-        <span></span>
-        <button>Enviar</button>
-      </QuizFrame>
-    </>
-  );
+import api from "../../services/api";
+
+export default class Quiz extends Component {
+  state = {
+    questions: [],
+    quiz: 1
+  };
+
+  componentDidMount() {
+    this.loadQuestions();
+  }
+
+  loadQuestions = async () => {
+    const response = await api.get("/questions");
+
+    const questions = response;
+
+    console.log(questions);
+    // this.setState({ questions : docs, productInfo, page})
+
+    // console.log(this.state.products)
+    // console.log(this.state.productInfo)
+  };
+
+  render() {
+    return (
+      <>
+        <ImageFrame>
+          <img src="" alt="" />
+          <img src="" alt="" />
+        </ImageFrame>
+        <QuizFrame>
+          <Question>
+            <p>amsdasd</p>
+          </Question>
+          <Answers>
+            <li>a</li>
+            <li>b</li>
+            <li>c</li>
+            <li>d</li>
+          </Answers>
+          <Code>soma</Code>
+          <span></span>
+          <button>Enviar</button>
+        </QuizFrame>
+      </>
+    );
+  }
 }
---
+++ src/pages/Quiz/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const ImageFrame = styled.form``;
+
+export const QuizFrame = styled.form``;
+
+export const Question = styled.form``;
+
+export const Answers = styled.form``;
+
+export const Code = styled.form``;
